4c2d51bf17a5e8931ae87ac6edbec47513544d75
João Lourenço <jlourenco5691@gmail.com>
2023-01-25 16:30:44 +0000

    feat(exercise): add exercise to introduce variable declarations
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

---
+++ exercises/variables/variables1.d
@@ -0,0 +1,12 @@
+/// Make the following program compile!
+/// Run `dlings hint variables1` or type 'hint' if in watch mode.
+
+import std.stdio;
+
+// Just D it
+
+void main()
+{
+	a = 3;
+	writefln!"a + 3 = %s"(a + 3);
+}
