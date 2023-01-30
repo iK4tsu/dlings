11eabbfa981d285d6cf7570dcc401f29f697e33d
João Lourenço <jlourenco5691@gmail.com>
2023-01-30 23:41:19 +0000

    feat(exercise): add functions chapter
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

---
+++ exercises/functions/functions2.d
@@ -0,0 +1,16 @@
+/// Make me compile!
+/// Run `dlings hint variables1` or type 'hint' if in watch mode.
+
+// Just D it
+
+import std.stdio;
+
+float half()
+{
+	return a / 2f; // Do not change this line!
+}
+
+void main()
+{
+	writeln(half(5));
+}
