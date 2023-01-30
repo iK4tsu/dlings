dddc836c053876bc6b7d22728c425672ef1e0018
João Lourenço <jlourenco5691@gmail.com>
2023-01-30 23:53:33 +0000

    feat(exercise): add variable assign exercise before 'variables3'
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- exercises/variables/variables3.d
+++ exercises/variables/variables3.d
@@ -1,25 +1,14 @@
-/// Variables in D are mutable by default. But sometimes we want to express
-/// them otherwise and make their values unchangeable.
+/// Variables are mutable by default in D. This means that their value can be
+/// changed at any time.
 
-/// There are 3 ways to make this happen:
-/// * enum constants
-/// * const
-/// * immutable
-
-/// Working with constant values also allows us to work with CTFE - Compile Time
-/// Function Evaluation - a D feature that allows any function to execute at
-/// compile time! We will get more into this in future chapters.
-
-/// Since constant values cannot change, once they're initialized, their state
-/// remains the same until its lifetime is over.
-
-import std.stdio;
+/// Run `dlings hint variables1` or type 'hint' if in watch mode.
 
 // Just D it
 
-void main()
+unittest
 {
-	const a = 5; // notice 'a' being infered as `const int`
-	a = 10;
-	writefln!"a is %s"(a);
+	// Without modifying existing code, define logic that will allow the
+	// expression `i == 4` to be true.
+	int i;
+	assert(i == 4);
 }
